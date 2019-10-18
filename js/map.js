d61088f32ed12687152ad79ebba7a75da205d5c9
Aleks <osinaleksandr1989@gmail.com>
2019-10-18 22:07:08 +0500

    добавил условие выхода за пределы карты

--- js/map.js
+++ js/map.js
@@ -6,10 +6,12 @@
   var formFieldSets = adForm.querySelectorAll('fieldset');
   var mainPin = document.querySelector('.map__pin--main');
   var inputAddress = document.querySelector('#address');
+  var startTrail = true;
   var disableMap = function () {
     for (var i = 0; i < formFieldSets.length; i++) {
       formFieldSets[i].setAttribute('disabled', 'disabled');
     }
+    startTrail = false;
     map.classList.add('map--faded');
     adForm.classList.add('ad-form--disabled');
     mapFilters.classList.add('map__filters--disabled');
@@ -22,57 +24,72 @@
     adForm.classList.remove('ad-form--disabled');
     mapFilters.classList.remove('map__filters--disabled');
   };
-  var setAddress = function () {
-    inputAddress.value = (parseFloat(mainPin.style.left) + window.utils.MAIN_PIN_X) + ',' + (parseFloat(mainPin.style.top) + window.utils.MAIN_PIN_Y);
+  var setAddress = function (x, y) {
+    inputAddress.value = (parseFloat(x) + window.utils.MAIN_PIN_X) + ',' + (parseFloat(y) + window.utils.MAIN_PIN_Y);
     return inputAddress.value;
   };
   var activatePage = function () {
-    setAddress();
+    startTrail = true;
+    setAddress(mainPin.style.left, mainPin.style.top);
     enableMap();
     window.pin.getAllPins(8);
     mainPin.removeEventListener('click', activatePage);
   };
   disableMap();
   mainPin.addEventListener('click', activatePage);
+  if (startTrail) {
+    mainPin.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
+      var startCoords = {
+        x: evt.clientX,
+        y: evt.clientY
+      };
+      var stopTrail = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        document.removeEventListener('mouseup', onMouseUp);
+      };
+      var onMouseMove = function (moveEvt) {
+        moveEvt.preventDefault();
+        var shift = {
+          x: startCoords.x - moveEvt.clientX,
+          y: startCoords.y - moveEvt.clientY
+        };
+
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+        var mouseCoordsY = mainPin.offsetTop - shift.y;
+        var mouseCoordsX = mainPin.offsetLeft - shift.x;
+        if (mouseCoordsY <= 160) {
+          mouseCoordsY = 160;
+          stopTrail();
+        } else if (mouseCoordsY >= 625) {
+          mouseCoordsY = 625;
+          stopTrail();
+        }
+        if (mouseCoordsX <= 0) {
+          mouseCoordsX = 0;
+          stopTrail();
+        } else if (mouseCoordsX >= 1140) {
+          mouseCoordsX = 1140;
+          stopTrail();
+        }
+        mainPin.style.top = mouseCoordsY + 'px';
+        mainPin.style.left = mouseCoordsX + 'px';
+        setAddress(mainPin.style.left, mainPin.style.top);
+      };
+
+      var onMouseUp = function (upEvt) {
+        upEvt.preventDefault();
+        stopTrail();
+      };
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+    });
+  }
   window.map = {
     map: map
   };
-  mainPin.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-    var startCoords = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-    var onMouseOutMap = function () {
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-    };
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      var shift = {
-        x: startCoords.x - moveEvt.clientX,
-        y: startCoords.y - moveEvt.clientY
-      };
-
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
-      mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
-      setAddress();
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-      window.pin.pinSection.removeEventListener('mouseleave', onMouseOutMap);
-    };
-    window.pin.pinSection.addEventListener('mouseleave', onMouseOutMap);
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
 })();
+
