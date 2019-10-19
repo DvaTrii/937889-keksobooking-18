348bc389085dc967f245568ae26db4f30ec3f43e
Aleks <osinaleksandr1989@gmail.com>
2019-10-19 16:35:51 +0500

    исправил замечания

--- js/map.js
+++ js/map.js
@@ -6,11 +6,10 @@
   var formFieldSets = adForm.querySelectorAll('fieldset');
   var mainPin = document.querySelector('.map__pin--main');
   var inputAddress = document.querySelector('#address');
-  var startTrail = false;
   var disableMap = function () {
-    for (var i = 0; i < formFieldSets.length; i++) {
-      formFieldSets[i].setAttribute('disabled', 'disabled');
-    }
+    formFieldSets.forEach(function (it) {
+      it.setAttribute('disabled', 'disabled');
+    });
     map.classList.add('map--faded');
     adForm.classList.add('ad-form--disabled');
     mapFilters.classList.add('map__filters--disabled');
@@ -19,7 +18,6 @@
     for (var i = 0; i < formFieldSets.length; i++) {
       formFieldSets[i].removeAttribute('disabled');
     }
-    startTrail = true;
     map.classList.remove('map--faded');
     adForm.classList.remove('ad-form--disabled');
     mapFilters.classList.remove('map__filters--disabled');
@@ -28,70 +26,57 @@
     inputAddress.value = (parseFloat(x) + window.utils.MAIN_PIN_X) + ',' + (parseFloat(y) + window.utils.MAIN_PIN_Y);
     return inputAddress.value;
   };
-  var movePin = function () {
-    if (startTrail) {
-      mainPin.addEventListener('mousedown', function (evt) {
-        evt.preventDefault();
-        var startCoords = {
-          x: evt.clientX,
-          y: evt.clientY
-        };
-        var stopTrail = function () {
-          document.removeEventListener('mousemove', onMouseMove);
-          document.removeEventListener('mouseup', onMouseUp);
-        };
-        var onMouseMove = function (moveEvt) {
-          moveEvt.preventDefault();
-          var shift = {
-            x: startCoords.x - moveEvt.clientX,
-            y: startCoords.y - moveEvt.clientY
-          };
 
-          startCoords = {
-            x: moveEvt.clientX,
-            y: moveEvt.clientY
-          };
-          var mouseCoordsY = mainPin.offsetTop - shift.y;
-          var mouseCoordsX = mainPin.offsetLeft - shift.x;
-          if (mouseCoordsY <= 155) {
-            mouseCoordsY = 155;
-            stopTrail();
-          } else if (mouseCoordsY >= 620) {
-            mouseCoordsY = 620;
-            stopTrail();
-          }
-          if (mouseCoordsX <= 5) {
-            mouseCoordsX = 5;
-            stopTrail();
-          } else if (mouseCoordsX >= 1130) {
-            mouseCoordsX = 1130;
-            stopTrail();
-          }
-          mainPin.style.top = mouseCoordsY + 'px';
-          mainPin.style.left = mouseCoordsX + 'px';
-          setAddress(mainPin.style.left, mainPin.style.top);
-        };
-
-        var onMouseUp = function (upEvt) {
-          upEvt.preventDefault();
-          stopTrail();
-        };
-        document.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-      });
-    }
-  };
   var activatePage = function () {
     setAddress(mainPin.style.left, mainPin.style.top);
     enableMap();
-    movePin();
     window.pin.getAllPins(8);
-    mainPin.removeEventListener('click', activatePage);
+    mainPin.removeEventListener('mousedown', activatePage);
   };
   disableMap();
-  mainPin.addEventListener('click', activatePage);
+  mainPin.addEventListener('mousedown', activatePage);
+  mainPin.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      var mouseCoordsY = mainPin.offsetTop - shift.y;
+      var mouseCoordsX = mainPin.offsetLeft - shift.x;
+      if (mouseCoordsY <= window.utils.MAP_BORDER_START_Y) {
+        mouseCoordsY = window.utils.MAP_BORDER_START_Y;
+      } else if (mouseCoordsY >= window.utils.MAP_BORDER_END_Y) {
+        mouseCoordsY = window.utils.MAP_BORDER_END_Y;
+      }
+      if (mouseCoordsX <= window.utils.MAP_BORDER_START_X) {
+        mouseCoordsX = window.utils.MAP_BORDER_START_X;
+      } else if (mouseCoordsX >= window.utils.MAP_BORDER_END_X) {
+        mouseCoordsX = window.utils.MAP_BORDER_END_X;
+      }
+      mainPin.style.top = mouseCoordsY + 'px';
+      mainPin.style.left = mouseCoordsX + 'px';
+      setAddress(mainPin.style.left, mainPin.style.top);
+    };
+
+    var onMouseUp = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
   window.map = {
     map: map
   };
 })();
-
--- js/utils.js
+++ js/utils.js
@@ -7,7 +7,7 @@
   var getRandomIntInclusive = function (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; // Максимум и минимум включаются
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   };
   var getRandomArray = function (arrayName) {
     var randomArray = [];
@@ -17,6 +17,10 @@
     return randomArray;
   };
   window.utils = {
+    MAP_BORDER_START_X: 5,
+    MAP_BORDER_END_X: 1130,
+    MAP_BORDER_START_Y: 155,
+    MAP_BORDER_END_Y: 620,
     MAIN_PIN_X: 22.5,
     MAIN_PIN_Y: 65,
     ESC_KEYCODE: 27,
